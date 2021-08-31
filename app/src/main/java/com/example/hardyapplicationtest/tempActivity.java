2e9a3ca70c75cbe6975fa6182fd70af61768d666
Umang Patel <id.umang@gmail.com>
2021-08-31 16:53:08 +0530

    Add Print Line

--- app/src/main/java/com/example/hardyapplicationtest/tempActivity.java
+++ app/src/main/java/com/example/hardyapplicationtest/tempActivity.java
@@ -10,5 +10,6 @@ public class tempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+        System.out.println("I have Completed Understand Basic Git");
     }
 }
